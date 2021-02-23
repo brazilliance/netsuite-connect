6b5c662c864fea45f631418ba1e166188584e01a
Miquel Brazil <miquel@brazilliance.co>
2021-02-22 21:00:40 -0500

    add: initial project components
    Add the initial components that comprise the NetSuite Connect package.

---
+++ src/Signature/HmacSha256Signature.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NSConnect\Signature;
+
+class HmacSha256Signature
+{
+    /**
+     * @inheritDoc
+     */
+    public function method()
+    {
+        // TODO: Implement method() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function sign($uri, array $parameters = [], $method = 'POST')
+    {
+        // TODO: Implement sign() method.
+    }
+}
